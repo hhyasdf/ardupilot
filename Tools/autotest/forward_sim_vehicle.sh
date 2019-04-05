3ee906603ea8a1b7dcd8d8c573c057576ff5a69a
hhyasdf <552483776@qq.com>
2019-04-05 17:12:20 +0800

    update simulate shell script

--- Tools/autotest/forward_sim_vehicle.sh
+++ Tools/autotest/forward_sim_vehicle.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
-if [ "$#" -eq "2"  ];then
-    NEW_SYSTEM_ID=$1
-    DEST_IP=$2
+NEW_SYSTEM_ID=$1
+DEST_IP=$2
 
-    cd /ardupilot/APMrover2/
+cd /ardupilot/APMrover2/
 
-    CONTENT=$(grep -Eo "MAV_SYSTEM_ID.+" ./config.h)
-    sed -i "s/$CONTENT/MAV_SYSTEM_ID    $NEW_SYSTEM_ID/g" ./config.h
+CONTENT=$(grep -Eo "MAV_SYSTEM_ID.+" ./config.h)
+sed -i "s/$CONTENT/MAV_SYSTEM_ID    $NEW_SYSTEM_ID/g" ./config.h
 
+if [ "$#" -eq "2" ];then
     sim_vehicle.py --out=udp:$DEST_IP:14550
+elif [ "$#" -eq "1" ];then
+    echo "mavproxy will not start, the UAV will listen to tcp:127.0.0.1:5760"
+    echo "<<<<<<<<<<<<<<<<<<<<<<<"
+    sim_vehicle.py --no-mavproxy
 else
     echo "two parameters (a mavlink system id and a dest ip) are required!"
 fi
